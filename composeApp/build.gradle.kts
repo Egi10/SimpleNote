81d87e0ab38714c802358341d0c848e5daea3aae
Egi10 <egifcb@gmail.com>
2025-05-17 20:29:55 +0700

    Refactor: Update dependencies and Gradle version
    - Updated various library versions in `gradle/libs.versions.toml`, including AGP, Kotlin, Compose, and others.
    - Added `compose.materialIconsExtended` to `composeApp/build.gradle.kts`.
    - Upgraded Gradle wrapper to version 8.11.1.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,7 +1,6 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
@@ -62,6 +61,7 @@ kotlin {
             implementation(compose.foundation)
             implementation(compose.material)
             implementation(compose.ui)
+            implementation(compose.materialIconsExtended)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
             // Navigation
